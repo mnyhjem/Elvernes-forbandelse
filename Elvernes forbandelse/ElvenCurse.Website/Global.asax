60e1cad75dd1a88803ae50bd81f4ff54d0026c3a
Martin <email@martinnyhjem.dk>
2016-08-14 22:29:20 +0200

    Så..

---
+++ Elvernes forbandelse/ElvenCurse.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElvenCurse.Website.MvcApplication" Language="C#" %>
---
+++ Elvernes forbandelse/ElvenCurse.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElvenCurse.Website.Startup))]
+namespace ElvenCurse.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
